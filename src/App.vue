055690c92a9e2a915bf5d0f39e376b3742e5c514
ErwinPrograms <erwinprograms@gmail.com>
2024-06-12 19:42:45 -0700

    Experiment with handling json data in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<template>
-    <main></main>
-</template>
-
 <script setup>
 import { ref } from 'vue';
+// Placeholder fetching data from a json file
+import json from './data.json'
+import CommentList from './components/CommentSection/CommentList.vue';
+
+const data = ref(json)
+const currentUser = ref(json.currentUser)
+const allComments = ref(json.comments)
 </script>
+
+
+<template>
+    <main>
+        <div v-for="(value, key) in currentUser"> {{ key }} : {{ value }} </div>
+        <!-- <div v-for="comment in allComments">
+            <div v-for="(value, key) in comment"> {{ key }} : {{ value }} 
+                <template v-if="key == 'replies'">
+                    <div v-for="reply in value"> {{ reply }} </div>
+                </template>
+            </div>
+        </div> -->
+        <CommentList :allComments="allComments"/>
+    </main>
+</template>
---
+++ src/components/CommentSection/CommentList.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(['allComments'])
+</script>
+
+<template>
+    <h1>Hi, I'm CommentList</h1>
+    <div v-for="comment in allComments">
+        <div v-for="(value, key) in comment"> {{ key }} : {{ value }} </div>
+    </div>
+</template>
+
