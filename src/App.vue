eb58056f385c64e1de85698feaef7448fcb62f42
ErwinPrograms <erwinprograms@gmail.com>
2024-06-12 20:02:51 -0700

    Add data json importing and connecting props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<template>
-    <main></main>
-</template>
-
 <script setup>
 import { ref } from 'vue';
+import json from './data.json'
+import CommentList from './components/CommentSection/CommentList.vue';
+
+const allComments = ref(json.comments)
+const currentUser = ref(json.currentUser)
 </script>
+
+<template>
+    <main>
+        <CommentList :allComments="allComments" />
+    </main>
+</template>
---
+++ src/components/CommentSection/CommentList.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+    allComments: {
+        type: Array,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div v-for="(comment, index) in allComments" :key="index">
+        {{ comment }} 
+    </div>
+</template>
